4a75bb4cfb79feee237c722abfdb8b9afedad299
cktalala <boompetbank@gmail.com>
2026-05-22 17:50:18 +0700

    feat: wrap ExplorePlan component in Suspense for improved loading handling

--- src/app/explore-plan/page.tsx
+++ src/app/explore-plan/page.tsx
@@ -1,5 +1,10 @@
+import { Suspense } from "react";
 import ExplorePlan from "@/src/constants/explorPlan";
 
 export default function ExplorePlanPage() {
-  return <ExplorePlan />;
+  return (
+    <Suspense fallback={null}>
+      <ExplorePlan />
+    </Suspense>
+  );
 }
