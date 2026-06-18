a4519cfb477326f8df4f4112fbb5201fad549a22
cktalala <boompetbank@gmail.com>
2026-06-18 16:11:35 +0700

    refactor: adjust base path configuration in next.config.ts to conditionally include site name for improved routing

--- next.config.ts
+++ next.config.ts
@@ -5,7 +5,9 @@ const nextConfig: NextConfig = {
   trailingSlash: true,
   basePath: process.env.NEXT_PUBLIC_BASE_PATH
     ? `${process.env.NEXT_PUBLIC_BASE_PATH}/${process.env.NEXT_PUBLIC_SITE}`
-    : "",
+    : process.env.NEXT_PUBLIC_SITE
+      ? `/${process.env.NEXT_PUBLIC_SITE}`
+      : "",
   transpilePackages: ["@wevisdemo/ui"],
   images: {
     unoptimized: true,
